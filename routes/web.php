ed095107f3739fd02e32e093f992b33aadec73b3
iloveleon19 <iloveleon19@gmail.com>
2018-09-03 16:00:22 +0800

    finish

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
  
 Route::resource('/task', 'TaskController');
+
+Auth::routes();
